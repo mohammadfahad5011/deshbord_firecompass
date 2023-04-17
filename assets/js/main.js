2fd546d2d700107a98d93aef06eef716ac6eda3f
mohammadfahad5011 <mohammadfahad5011@gmail.com>
2023-04-17 23:33:01 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Sidebar Activation Script
+
+// document.getElementById("sidebarOpen").addEventListener("click", function () {
+//     document.getElementById("sidebar").classList.add("show");
+// });
+
+// document.getElementById("sidebarClose").addEventListener("click", function () {
+//     document.getElementById("sidebar").classList.remove("show");
+// });
